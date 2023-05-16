1ee1a7ba92ef0a887892de2e78d141f14888a501
weifashi <605403358@qq.com>
2023-05-16 10:29:21 +0800

    build

---
+++ public/js/build/preview.3af367ba.js
@@ -0,0 +1,1 @@
+import"./app.3bd45d5d.js";import{p as a}from"./app.3bd45d5d.js";export{a as default};
--- public/js/build/preview.92a44621.js
+++
@@ -1,1 +0,0 @@
-import"./app.a335529f.js";import{p as a}from"./app.a335529f.js";export{a as default};
